1cc68a593460b04f472849464bb1a6a4b2bc3baa
Subi <subi.philip@gmail.com>
2021-10-11 13:20:40 -0500

    remvoed Id

--- routes/activity.js
+++ routes/activity.js
@@ -21,7 +21,6 @@ exports.execute = async (req, res) => {
     await SFClient.saveData(process.env.DATA_EXTENSION_EXTERNAL_KEY, [
       {
         keys: {
-          Id: id,
           SubscriberKey: data.inArguments[0].contactKey,
         },
         values: {
