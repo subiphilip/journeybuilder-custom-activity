b1af44c8f1bef7f529822f90270f00df3a9fe768
Subi <subi.philip@gmail.com>
2021-10-11 13:04:15 -0500

    updated log

--- routes/activity.js
+++ routes/activity.js
@@ -31,7 +31,7 @@ exports.execute = async (req, res) => {
       },
     ]);
 
-    logger.info('SFClient Done Save. id='+id+' SubscriberKey='+data.inArguments[0].contactKey+' Event='+data.inArguments[0].DropdownOptions+' text='+data.inArguments[0].Text);
+    logger.info('SFClient Done Save Log. id='+id+' SubscriberKey='+data.inArguments[0].contactKey+' Event='+data.inArguments[0].DropdownOptions+' text='+data.inArguments[0].Text);
   } catch (error) {
     logger.error(error);
   }
